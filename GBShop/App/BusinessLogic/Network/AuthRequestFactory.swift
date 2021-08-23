a7aa21a5f52936cb0a8c446fa2780ba6366df9c8
Dmitriy <uselessgod90@gmail.com>
2021-08-23 19:33:32 +0300

    hw2

---
+++ GBShop/App/BusinessLogic/Network/AuthRequestFactory.swift
@@ -0,0 +1,14 @@
+//
+//  AuthRequestFactory.swift
+//  GBShop
+//
+//  Created by Дмитрий Емельянов on 23.08.2021.
+//
+
+import Foundation
+
+import Alamofire
+
+protocol AuthRequestFactory {
+    func login(userName: String, password: String, completionHandler: @escaping (AFDataResponse<LoginResult>) -> Void)
+}
---
+++ GBShop/Core/Network/DataRequest.swift
@@ -0,0 +1,35 @@
+//
+//  DataRequest.swift
+//  GBShop
+//
+//  Created by Дмитрий Емельянов on 23.08.2021.
+//
+
+import Foundation
+import Alamofire
+
+protocol AbstractRequestFactory {
+    var errorParser: AbstractErrorParser { get }
+    var sessionManager: Session { get }
+    var queue: DispatchQueue { get }
+    
+    @discardableResult
+    func request<T: Decodable>(
+        request: URLRequestConvertible,
+        completionHandler: @escaping (AFDataResponse<T>) -> Void)
+        -> DataRequest
+}
+
+extension AbstractRequestFactory {
+    
+    @discardableResult
+    public func request<T: Decodable>(
+        request: URLRequestConvertible,
+        completionHandler: @escaping (AFDataResponse<T>) -> Void)
+        -> DataRequest {
+            return sessionManager
+                .request(request)
+                .responseCodable(errorParser: errorParser, queue: queue, completionHandler: completionHandler)
+    }
+}
+
